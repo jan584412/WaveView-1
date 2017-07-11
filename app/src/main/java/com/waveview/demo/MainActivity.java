5f9a8394432874c05152e15ea3133333a9b6034b
Thi An Dac <thiandac@gmail.com>
2017-07-11 22:41:45 +0800

    change

--- app/src/main/java/com/waveview/demo/MainActivity.java
+++ app/src/main/java/com/waveview/demo/MainActivity.java
@@ -2,7 +2,10 @@ package com.waveview.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.john.waveview.WaveView;
 
@@ -11,20 +14,41 @@ import com.john.waveview.WaveView;
  */
 public class MainActivity extends Activity {
 
-    private SeekBar seekBar;
-    private WaveView waveView;
+    private SeekBar seekBar,seekBar1;
+    private WaveView waveView,waveView1;
+    private TextView textView;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        textView = (TextView) findViewById(R.id.editText);
         seekBar = (SeekBar) findViewById(R.id.seek_bar);
+        seekBar1 = (SeekBar) findViewById(R.id.seek_bar1);
         waveView = (WaveView) findViewById(R.id.wave_view);
-
+        waveView1 = (WaveView) findViewById(R.id.wave_view1);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 waveView.setProgress(progress);
+                textView.setText(String.valueOf(progress));
+                Log.i("s",String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                waveView1.setProgress(progress);
             }
 
             @Override
